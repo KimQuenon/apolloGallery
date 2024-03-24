ff63e518c3683881e5f82f79ee8dc32d6c9bacd2
Kim <kimberley.quenon.@gmail.com>
2024-03-24 18:47:35 +0100

    first commit - install

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,7 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
