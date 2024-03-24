5a1ed748a2c4f0bc34ad5bd1c6d131de755ee6c5
Kim <kimberley.quenon.@gmail.com>
2024-03-24 19:15:57 +0100

    individual display

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,6 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-import './styles/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle'
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import './styles/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
--- src/Controller/ArtworkController.php
+++ src/Controller/ArtworkController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Artwork;
 use App\Repository\ArtworkRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArtworkController extends AbstractController
@@ -16,6 +18,14 @@ class ArtworkController extends AbstractController
 
         return $this->render('artworks/index.html.twig', [
             'artworks' => $artworks,
+        ]);
+    }
+
+    #[Route("/artworks/{slug}", name: "artworks_show")]
+    public function show(#[MapEntity(mapping: ['slug' => 'slug'])] Artwork $artwork): Response
+    {
+        return $this->render("artworks/show.html.twig", [
+            'artwork' => $artwork,
         ]);
     }
 }
