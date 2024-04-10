efc65347998255ff5ed07ca4fdb30720e6223f20
Kim <kimberley.quenon.@gmail.com>
2024-04-10 21:30:23 +0200

    DESIGN - Homepage Slide 1 (responsive)

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -9,70 +9,72 @@ gsap.registerPlugin(ScrollTrigger);
             document.body.style.height = `${totalBodyHeight}px`;
         });
 
-        ScrollTrigger.create({
+        const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+        if (mediaQuery.matches) {
+          ScrollTrigger.disable();
+        } else {
+          ScrollTrigger.create({
             trigger: ".website-content",
             start: "-0.1% top",
             end: "bottom bottom",
-            onEnter: () =>{
-                gsap.set(".website-content", {position: 'absolute', top:"200%"});
+            onEnter: () => {
+              gsap.set(".website-content", { position: "absolute", top: "200%" });
             },
-            onLeaveBack: () =>{
-                gsap.set(".website-content", {position: 'fixed', top:"0"});
+            onLeaveBack: () => {
+              gsap.set(".website-content", { position: "fixed", top: "0" });
             },
-        })
-
-        gsap.to(".header .letters:first-child",{
-            x:()=> -innerWidth *3,
+          });
+        
+          gsap.to(".header .letters:first-child", {
+            x: () => -innerWidth * 3,
             scale: 10,
             ease: "power2.inOut",
             scrollTrigger: {
-                start: "top top",
-                end: `+=200%`,
-                scrub:1
-            }
-        });
-
-        gsap.to(".scroll", {
+              start: "top top",
+              end: `+=200%`,
+              scrub: 1,
+            },
+          });
+        
+          gsap.to(".scroll", {
             y: () => window.innerHeight * 3,
             scale: 1,
             ease: "power2.inOut",
             scrollTrigger: {
-                trigger: ".scroll",
-                start: "bottom bottom",
-                end: `+=50%`,
-                scrub: 1,
-            }
-        });
-
-        // gsap.to(".header .letters:last-child",{
-        //     x:()=> innerWidth *3,
-        //     scale: 10,
-        //     ease: "power2.inOut",
-        //     scrollTrigger: {
-        //         start: "top top",
-        //         end: `+=200%`,
-        //         scrub:1
-        //     }
-        // });
-
-        gsap.to(".img-holder",{
+              trigger: ".scroll",
+              start: "bottom bottom",
+              end: `+=50%`,
+              scrub: 1,
+            },
+          });
+        
+          gsap.to(".img-holder", {
             scale: 2.5,
-            clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
             ease: "power2.inOut",
             scrollTrigger: {
-                start: "top top",
-                end: `+=200%`,
-                scrub:1
-            }   
-        });
-
-        gsap.to(".img-holder img",{
+              start: "top top",
+              end: `+=200%`,
+              scrub: 1,
+            },
+          });
+        
+          gsap.to(".img-holder img", {
             scale: 1,
             ease: "power2.inOut",
             scrollTrigger: {
-                start: "top top",
-                end: `+=200%`,
-                scrub:1
-            }   
-        });
+              start: "top top",
+              end: `+=200%`,
+              scrub: 1,
+            },
+          });
+        }
         
+        mediaQuery.addListener((event) => {
+          if (event.matches) {
+            ScrollTrigger.disable();
+          } else {
+            ScrollTrigger.enable();
+          }
+        });
