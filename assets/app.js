16e35bbf2769cab9b324f35b67b572c330f8bf54
Kim <kimberley.quenon.@gmail.com>
2024-04-19 21:31:21 +0200

    scss externalization

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,11 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.scss';
+import './styles/about.scss';
+import './styles/navigation.scss';
+import './styles/_colors.scss';
+import './styles/_fonts.scss';
+import './styles/_mixins.scss';
 
 import './styles/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
